8b5a3da4390b8da8ee2fe4dd044b302a8e44d315
IkhsanRachmat <23ikhsanrachmat@gmail.com>
2022-10-26 23:21:46 +0700

    Menyelesaikan vidio ke 9 tentang Database Seeder

--- database/migrations/2022_10_25_094445_create_posts_table.php
+++ database/migrations/2022_10_25_094445_create_posts_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class CreatePostsTable extends Migration
 {
     /**
@@ -16,14 +14,15 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id');
+            $table->foreignId('user_id');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('excerpt');
+            $table->text('body');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
