b008b7f8bfe9bc294365f42ee2629cafe55b6a4b
IkhsanRachmat <23ikhsanrachmat@gmail.com>
2022-10-26 22:05:14 +0700

    Mengulang video3 Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Ikhsan rachmat",
+        "email" => "Ikhsanrachmat@gmail.com",
+        "image" => "wow.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
